fe7a10b5930dad5532d82da087483e120ce6b5ad
Abhi <abhishek7340128579@gmail.com>
2021-05-05 11:54:35 +0530

    javaDoc added

--- app/src/main/java/com/example/mycounter/MainActivity.java
+++ app/src/main/java/com/example/mycounter/MainActivity.java
@@ -8,6 +8,10 @@ import android.widget.Toast;
 
 import com.example.mycounter.databinding.ActivityMainBinding;
 
+/**
+ *
+ */
+
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding b;
     int qty=0;
@@ -15,42 +19,60 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Initializing the binding
         b=ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(b.getRoot());
 
+        //Handel click events
         eventHandler();
     }
 
-
+    /**
+     * Trigger Event handlers to listen the actions
+     */
     private void eventHandler() {
+        //click listener on Decrease button
         b.decBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //To decrease quantity
                 decreaseQuantity();
             }
         });
 
+        //click listener on Increase button
         b.incBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //To increase quantity
                 increaseQuantity();
             }
         });
     }
 
-    //To increase the quantity
+    /**
+     * Increase the quantity
+     * update TextView
+     */
     private void increaseQuantity() {
+        //update quantity and textView
         b.qty.setText(++qty+"");
     }
 
-    //To decrease the quantity
+    /**
+     * Decrease the quantity
+     * update TextView
+     */
     private void decreaseQuantity() {
 
+        //check quantity
         if(qty==0){
+            //show toast message
             Toast.makeText(this, "Quantity is already 0", Toast.LENGTH_SHORT).show();
             return;
         }
 
+        //update quantity and textView
         b.qty.setText(--qty+"");
     }
 
